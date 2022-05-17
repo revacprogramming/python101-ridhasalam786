537f0bb163d4b1af6f8bd03c4e61de7108ea36a4
Ridha Salam <96277953+ridhasalam786@users.noreply.github.com>
2022-05-17 17:32:20 +0000

    problem#09.py

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,15 @@
 # Lists
 
 filename = "dataset/romeo.txt"
+fh = open(filename)
+lst = list()
+for line in fh:
+    line.rstrip()
+    w=line.split()
+    for i in w:
+        if i in lst:
+            continue
+        else:
+            lst.append(i)
+lst.sort()
+print(lst)
