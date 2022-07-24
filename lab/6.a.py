399bd18effd03b9bc98a4a37793986826a306d76
Ridha Salam <96277953+ridhasalam786@users.noreply.github.com>
2022-07-24 03:57:55 +0000

    6.a.py

---
+++ lab/6.a.py
@@ -0,0 +1,24 @@
+def READ_DATA(r,c,matrix):
+    for i in range(m):
+        for j in range(n):
+            matrix[i][j]=int(input())   
+
+import numpy
+m= int(input("\n Enter the row size of M "))
+n=int(input("\n Enter the column size of N"))
+
+matrix=numpy.ndarray(shape=(m,n), dtype=int)         #creating an array
+print("size: \n",matrix.size)
+print("shape: \n",matrix.shape)
+print("Dimensions: \n",matrix.ndim)
+print("\n Enter %d elements of %d x %d matrix" %(m*n,m,n))
+READ_DATA(m,n,matrix)
+print("\n %d x %d matrix are:"%(m,n))
+print(matrix)
+print("Diagonal Elements of matrix: ")
+dia=matrix.diagonal()
+print(dia)
+r=int(input("Enter row number to display: "))
+print(matrix[r])
+c=int(input("Enter column number to display: "))
+print(matrix[c])
